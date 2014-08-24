3590a6f14725a63cb7a02f0b41c38b02fe98c44e
Vasily Gotovko <vasily.gotovko@gmail.com>
2014-08-25 01:25:21 +0300

    Complete parser

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,6 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
+require 'uri'
 require 'pry'
+require 'csv'
 
 class Parser
 
@@ -11,9 +13,32 @@ class Parser
 
   def run
     # TODO validate url here
-
+    start = Time.now
     related_paths = CategoryPage.new(@url).product_paths
-    ProductPage.new(@url, related_path).product_data
+    data = ProductPage.new(@url, related_paths).product_data
+    save_data_to_file(data)
+
+    puts "Process is completed (~#{(Time.now - start).to_i} sec.)"
+    puts "See results in #{@filename}"
+  end
+
+  private
+
+  def save_data_to_file(data)
+    CSV.open(@filename, 'wb') do |csv|
+      csv << %w[Name Price Image Delivery SKU]
+
+      data.each do |line|
+        csv <<
+        [
+          line[:name],
+          line[:price],
+          line[:image],
+          line[:delivery],
+          line[:sku]
+        ]
+      end
+    end
   end
 
 end
@@ -27,7 +52,9 @@ class CategoryPage
   # parse **related** product paths
   def product_paths
     page = Nokogiri::HTML(open(@url))
-    page.xpath("//div[contains(@class, 'floating-content-box')]//div[contains(@class, 'gridbox')]/a/@href")
+    page.xpath("//div[contains(@class, 'floating-content-box')]
+                //div[contains(@class, 'gridbox')]
+                /a/@href").map(&:value)
   end
 end
 
@@ -36,9 +63,74 @@ class ProductPage
   def initialize(url, paths)
     @url = url
     @paths = paths
+
+    @domain = URI.parse(@url).scheme + '://' + URI.parse(@url).host
   end
 
   def product_data
+    @paths.map do |path|
+      read_product_page(path)
+    end.flatten
+  end
+
+  private
+
+  def read_product_page(path)
+    @current_path = path
+    page = Nokogiri::HTML(open(@domain + path))
+
+    name = product_name(page)
+    image = product_image(page)
+    items = product_items(page)
+
+    items.each do |item|
+      item[:name] = "#{name} - #{item[:pack]}"
+      item[:image] = URI.parse(@url).scheme + ':' + image
+    end
+
+    items
+  end
+
+  def product_name(page)
+    page.xpath("//div[contains(@class, 'content-box')]/div/div/h1").text
+  end
+
+  def product_image(page)
+    page.xpath("//div[contains(@class, 'content-box')]
+                /div[contains(@class, 'internal-row')][1]
+                /div[contains(@class, 'gridbox')][1]
+                //img/@src")[0].value
+  end
+
+  def product_items(page)
+    line_xpath = "//div[contains(@class, 'content-box')]
+                  /ul[@id='product_listing']
+                  /li[contains(@class, 'product')]"
+
+    packs = page.xpath(line_xpath + "//div[@class='title']/text()[normalize-space()]").map { |pack| pack.text.strip }
+    prices = page.xpath(line_xpath + "//div[@class='ours']/span/text()").map { |price| price.text }
+    deliveries = page.xpath(line_xpath + "/div/strong/text()").map { |delivery| delivery.text.strip }
+    sku = page.xpath(line_xpath + "//strong[@itemprop='sku']/text()").map { |item| item.text }
+
+    check_count_of_items(packs, prices, deliveries, sku)
+
+    packs.size.times.inject([]) do |result, i|
+      result <<
+        {
+          pack: packs[i],
+          price: prices[i],
+          delivery: deliveries[i],
+          sku: sku[i]
+        }
+      result
+    end
+  end
+
+  def check_count_of_items(*args)
+    sizes = args.map(&:size)
+    if sizes.min != sizes.max
+      puts "Something is wrong during parsing page #{@domain + path}"
+    end
   end
 
 end
